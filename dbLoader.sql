721af42f0d1a26db0ff680a379d9e82561b269e1
FlawlessPT <jotape919@gmail.com>
2020-12-27 03:18:31 +0000

    Updated db

--- dbLoader.sql
+++ dbLoader.sql
@@ -44,8 +44,8 @@ CREATE TABLE park (
 
 CREATE TABLE space (
 	id int NOT NULL AUTO_INCREMENT,
-	idPark INT NOT NULL,
 	isReservativa boolean NOT NULL,
+	idPark INT NOT NULL,
 	PRIMARY KEY (id),
 	FOREIGN KEY (idPark) REFERENCES park(id)
 );
@@ -68,13 +68,13 @@ CREATE TABLE history (
 	idSpace int NOT NULL,
 	idPaymentMethod INT NOT NULL,
 	PRIMARY KEY (id),
-	FOREIGN KEY (idSavedSpace) REFERENCES savedSpaces(id),
+	FOREIGN KEY (idSpace) REFERENCES space(id),
 	FOREIGN KEY (idPaymentMethod) REFERENCES paymentMethod(id)
 );
 
-INSERT INTO `user` (`id`, `name`, `username`, `password`, `contact`, `email`) VALUES
-(1, 'João', 'joao', '123', '999888777', 'email@email.pt'),
-(2, 'Test User', 'test', 'root', '123', 'email_Test@email.pt');
+INSERT INTO `user` (`id`, `name`, `username`, `password`, `contact`, `email`, `isAdmin`) VALUES
+(1, 'João', 'joao', '123', '999888777', 'email@email.pt', 0),
+(2, 'Test User', 'test', 'root', '123', 'email_Test@email.pt', 0);
 
 INSERT INTO `vehicule` (`id`, `name`, `plate`, `state`, `idUser`) VALUES
 (1, 'Toyota Supra', '11-11-11', 1, 1),
@@ -105,5 +105,5 @@ INSERT INTO `space` (`id`, `idPark`, `isReservativa`) VALUES
 (15, 3, 1);
 
 INSERT INTO `paymentmethod` (`id`, `name`, `description`, `idUser`) VALUES 
-(1, 'PayPal', 'email@gmail.com', '1'),
-(2, 'MBWay', '999888777', '1'),
+(1, 'PayPal', 'email@gmail.com', 1),
+(2, 'MBWay', '999888777', 1);
