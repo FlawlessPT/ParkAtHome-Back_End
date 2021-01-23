18700f4dd5aebb79e7507972848e8a9433458f0e
FlawlessPT <jotape919@gmail.com>
2021-01-23 01:59:53 +0000

    Added countParkSavedSpaces to getParkById

--- Parks/GetParkById.php
+++ Parks/GetParkById.php
@@ -24,24 +24,52 @@ $id = $obj->id;
 $query = "SELECT * FROM park WHERE id=$id";
 $result = mysqli_query($conn, $query);
 
-$response = array();
-
 if ($result) {
     if (mysqli_num_rows($result) > 0) {
-        $i = 0;
-        while ($row = mysqli_fetch_assoc($result)) {
-            $response[$i] = $row;
-            $i++;
+        if ($row = mysqli_fetch_assoc($result)) {
+            $row["totalSavedSpaces"] = countParkSavedSpaces($conn, $row["id"]);
+            $response = $row;
         }
 
         $finalObj = (object) ['message' => "success", 'park' => $response];
     } else {
-        $finalObj = (object) ['message' => "park_not_found", 'park' => $response];
+        $finalObj = (object) ['message' => "park_not_found"];
     }
 } else {
-    $finalObj = (object) ['message' => "error", 'park' => $response];
+    $finalObj = (object) ['message' => "error"];
 }
 
+function countParkSavedSpaces($conn, $parkId)
+{
+    $total = 0;
+
+    $query = "SELECT idSpace FROM liveSavedSpaces";
+    $result = mysqli_query($conn, $query);
+
+    $response = array();
+
+    if ($result) {
+        $i = 0;
+        while ($row = mysqli_fetch_assoc($result)) {
+            $response[$i] = $row["idSpace"];
+            $i++;
+
+            $idSpace = $row["idSpace"];
+
+            $query2 = "SELECT * FROM space WHERE id=$idSpace AND idPark=$parkId";
+            $result2 = mysqli_query($conn, $query2);
+
+            if ($result2) {
+                while ($row = mysqli_fetch_assoc($result2)) {
+                    $total++;
+                }
+            }
+        }
+    }
+
+
+    return $total;
+}
 
 echo json_encode($finalObj, JSON_PRETTY_PRINT);
 
